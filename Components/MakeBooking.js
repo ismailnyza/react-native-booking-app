7bb48bc9a03827e5d729f2157e295a71040f7dcf
ismailnyza <ismailnyzarsl@gmail.com>
2023-04-12 05:19:51 +0530

    added input validations

--- Components/MakeBooking.js
+++ Components/MakeBooking.js
@@ -1,5 +1,14 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Button, Alert, Platform , ImageBackground } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Alert,
+  Platform,
+  ImageBackground,
+  TextInput,
+} from "react-native";
 import DateTimePicker from "@react-native-community/datetimepicker";
 import { Picker } from "@react-native-picker/picker";
 
@@ -10,18 +19,22 @@ export default function MakeBookingScreen({ navigation }) {
   const [appointmentTime, setAppointmentTime] = useState(new Date());
   const [showDatePicker, setShowDatePicker] = useState(false);
   const [showTimePicker, setShowTimePicker] = useState(false);
+  const [customerName, setCustomerName] = useState("");
 
   const doctors = [
-    { key: 1, doctorName: "Dr. John Smith", doctorId: 1001, specialization: "Cardiology", location: "New York" },
-    { key: 2, doctorName: "Dr. Jane Doe", doctorId: 1002, specialization: "Pediatrics", location: "Los Angeles" },
-    { key: 3, doctorName: "Dr. Michael Johnson", doctorId: 1003, specialization: "Orthopedics", location: "Chicago" },
-    { key: 4, doctorName: "Dr. Sarah Lee", doctorId: 1004, specialization: "Dermatology", location: "San Francisco" },
-    { key: 5, doctorName: "Dr. David Chen", doctorId: 1005, specialization: "Obstetrics and Gynecology", location: "Houston" }
+    { key: 1, doctorName: "Dr. John Smith" },
+    { key: 2, doctorName: "Dr. Jane Doe" },
   ];
 
-  
   const handleDoctorChange = (value) => {
     setDoctor(value);
+  };
+
+  const handleCustomerNameChange = (event) => {
+    console.log(event);
+    if (event != null) {
+      setCustomerName(event);
+    }
   };
 
   const handleAppointmentDateChange = (event, selectedDate) => {
@@ -54,11 +67,33 @@ export default function MakeBookingScreen({ navigation }) {
     Alert.alert("Doctor is available!");
   };
   const handleBookingConfirmation = () => {
-    // Code to confirm booking goes here
-    // Navigate to the confirm payment screen
+    // Get current time and date
+    const currentDate = new Date();
+    const currentTime = currentDate.getTime();
+
+    if (!/^[a-zA-Z\s]*[a-zA-Z][a-zA-Z\s]*$/.test(customerName)) {
+      Alert.alert("Error", "Please enter a valid customer name.");
+      return;
+    }
+
+    if (!doctor) {
+      alert("Please select a doctor.");
+      return;
+    }
+
+    if (!appointmentTime) {
+      alert("Please select an appointment time.");
+      return;
+    }
+
+    // Check if appointment time is before current time
+    if (appointmentTime.getTime() <= currentTime) {
+      alert("Appointment time should be after current time.");
+      return;
+    }
 
     navigation.navigate("ConfirmBooking", {
-      customerName: "John Doe",
+      customerName: customerName,
       date: appointmentTime.toLocaleDateString(),
       time: appointmentTime.toLocaleTimeString([], {
         hour: "2-digit",
@@ -66,24 +101,38 @@ export default function MakeBookingScreen({ navigation }) {
       }),
       doctorName: doctor,
       hospital: "City Hospital",
-      totalCharge: 2000,
+      totalCharge: 2000.0,
     });
   };
 
   return (
     <ImageBackground
-        source={require("../assets/Admin-payments.png")} // Replace with your image source
-        style={styles.backgroundImage}
-        resizeMode="stretch"
-      >
-      <Text style={[styles.title, { fontFamily: "Poppins_700Bold" }]}>Make a booking</Text>
+      source={require("../assets/Admin-payments.png")} // Replace with your image source
+      style={styles.backgroundImage}
+      resizeMode="stretch"
+    >
+      <Text style={[styles.title, { fontFamily: "Poppins_700Bold" }]}>
+        Make a booking
+      </Text>
       <View style={styles.form}>
+        <Text style={styles.label}>Customer Name</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={handleCustomerNameChange}
+        />
+        <Text style={styles.label}></Text>
         <Text style={styles.label}>Doctor</Text>
-        <SelectList 
-        setSelected={(val) => setSelected(val)} 
-        data={doctors} 
-        save="value"
-    />
+        {/* here */}
+        <Picker selectedValue={doctor} onValueChange={handleDoctorChange}>
+          <Picker.Item label="Select a doctor" value={null} />
+          {doctors.map((doctor) => (
+            <Picker.Item
+              key={doctor.key}
+              label={doctor.doctorName}
+              value={doctor.key}
+            />
+          ))}
+        </Picker>
         <Text style={styles.dateTimeText}>
           {appointmentTime.toLocaleDateString()}
         </Text>
@@ -93,7 +142,6 @@ export default function MakeBookingScreen({ navigation }) {
             minute: "2-digit",
           })}
         </Text>
-
         <View style={styles.dateTimeContainer}>
           <View style={styles.dateTime}>
             <Text style={styles.label}>Appointment date</Text>
@@ -104,7 +152,6 @@ export default function MakeBookingScreen({ navigation }) {
             <Button title="Select time" onPress={showTimePickerModal} />
           </View>
         </View>
-
         {showDatePicker && (
           <DateTimePicker
             value={appointmentTime}
@@ -113,7 +160,6 @@ export default function MakeBookingScreen({ navigation }) {
             onChange={handleAppointmentDateChange}
           />
         )}
-
         {showTimePicker && (
           <DateTimePicker
             value={appointmentTime}
@@ -122,7 +168,12 @@ export default function MakeBookingScreen({ navigation }) {
             onChange={handleAppointmentTimeChange}
           />
         )}
-
+        <View style={styles.buttonContainer}>
+          <Button
+            title="Add Customer Details"
+            onPress={handleBookingConfirmation}
+          />
+        </View>
         <View style={styles.buttonContainer}>
           <Button title="Confirm booking" onPress={handleBookingConfirmation} />
         </View>
