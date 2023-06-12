1d8808a807eba23bc65d5c6020ed74f75507d137
ismailnyza <ismailnyzarsl@gmail.com>
2023-06-12 23:28:20 +0530

    dummy commit

--- Components/IpayForm.js
+++ Components/IpayForm.js
@@ -57,46 +57,95 @@ export default function IpayForm(props) {
     }
   };
 
-  const htmlContent =
-    `
-    <html>
-<body>
-<form method="POST" action="https://sandbox.ipay.lk/ipg/checkout">
-<input type="hidden" name="merchantWebToken" value="eyJhbGciOiJIUzUxMiJ9.eyJtaWQiOiIwMDAwMDQzNyJ9.QRjj-CMOM95XyEhhYPHcKE_wx1nAfpyLlCOCj6hqgSURevSLfWF2_kz7_lX4lrZE4tEPutKVNJCMqRI9mRG03Q"> 
-<input type="hidden" name="orderId" value="OID123456` +
-    patient.patientName +
-    `">
-<input type="hidden" name="returnUrl" value="http://mywebsite.com/return?orderId=OID123456">
-<input type="hidden" name="cancelUrl" value="http://mywebsite.com/cancel?orderId=OID123456">
-<table>
-<tr>
-<td>Total Amount</td>
-<td>:</td>
-<td><input type="text" name="totalAmount" value="750"></td>
-</tr>
-<tr>
-<td>Customer Name </td>
-<td>:</td>
-<td><input type="text" name="customerName" value="Ravindu Fernando"></td>
-</tr>
-<tr>
-<td>Customer Mobile</td>
-<td>:</td>
-<td><input type="text" name="customerPhone" value="0701234567"></td>
-</tr>
-<tr>
-<td>Customer Email</td>
-<td>:</td>
-<td><input type="text" name="customerEmail" value="myemail@mail.com"></td>
-</tr>
-</table>
-<br>
-<input type="submit" value="Checkout Now">
-</form>
-</body>
-</html>
+  const htmlContent = `
+  <html>
+    <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <style>
+        body {
+          margin: 0;
+          padding: 0;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: 100vh;
+          background-color: #f2f2f2;
+          font-family: Arial, sans-serif;
+        }
+        
+        form {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          padding: 20px;
+          background-color: #ffffff;
+          border-radius: 10px;
+          box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+          width: 80%;
+          max-width: 400px;
+        }
+        
+        table {
+          width: 100%;
+          margin-bottom: 20px;
+        }
+        
+        input[type="text"] {
+          width: 100%;
+          padding: 10px;
+          border-radius: 5px;
+          border: 1px solid #dddddd;
+          margin-bottom: 10px;
+          box-sizing: border-box;
+        }
+        
+        input[type="submit"] {
+          background-color: #000000;
+          color: #ffffff;
+          padding: 10px 20px;
+          border-radius: 20px;
+          border: none;
+          font-weight: bold;
+          cursor: pointer;
+          width: 100%;
+        }
+      </style>
+    </head>
+    <body>
+      <form method="POST" action="https://sandbox.ipay.lk/ipg/checkout">
+        <input type="hidden" name="merchantWebToken" value="eyJhbGciOiJIUzUxMiJ9.eyJtaWQiOiIwMDAwMDQzNyJ9.QRjj-CMOM95XyEhhYPHcKE_wx1nAfpyLlCOCj6hqgSURevSLfWF2_kz7_lX4lrZE4tEPutKVNJCMqRI9mRG03Q"> 
+        <input type="hidden" name="orderId" value={"OID" + uuid.v4()}">
+        <input type="hidden" name="returnUrl" value="http://mywebsite.com/return?orderId=OID123456">
+        <input type="hidden" name="cancelUrl" value="http://mywebsite.com/cancel?orderId=OID123456">
+        <table>
+          <tr>
+            <td>Total Amount</td>
+            <td>:</td>
+            <td><input type="text" name="totalAmount" value="750"></td>
+          </tr>
+          <tr>
+            <td>Customer Name</td>
+            <td>:</td>
+            <td><input type="text" name="customerName" value="Ravindu Fernando"></td>
+          </tr>
+          <tr>
+            <td>Customer Mobile</td>
+            <td>:</td>
+            <td><input type="text" name="customerPhone" value="0701234567"></td>
+          </tr>
+          <tr>
+            <td>Customer Email</td>
+            <td>:</td>
+            <td><input type="text" name="customerEmail" value="myemail@mail.com"></td>
+          </tr>
+        </table>
+        <input type="submit" value="Checkout Now">
+      </form>
+    </body>
+  </html>
+`;
 
-    `;
+
 
   return (
     <View style={styles.container}>
--- Components/MyBookings.js
+++ Components/MyBookings.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import { Alert } from "react-native"; // Import the Alert component
-
-import { FlatList, ImageBackground, Text, View, Linking } from "react-native";
+import { Alert } from "react-native";
+import { FlatList, Text, View, Linking } from "react-native";
 import { Button, Card, IconButton } from "react-native-paper";
 import { styles } from "../Styles/Styles";
 
@@ -13,14 +12,14 @@ function BookingItem({ booking, navigation }) {
     navigation.navigate("RefundDetails", { bookingData: booking });
   };
 
-  const cancelBooking = async (booking) => {
-    console.log(booking)
-    const endpoint = `http://192.168.122.1:9090/${booking.bookingId}`;
+  const cancelBooking = async (bookingId) => {
+    console.log(bookingId);
+    const endpoint = `http://192.168.122.1:9090/${bookingId}`;
     try {
       const response = await fetch(endpoint, {
         method: "DELETE",
       });
-      
+
       if (response.ok) {
         // Booking canceled successfully
         console.log("Booking canceled successfully");
@@ -31,13 +30,9 @@ function BookingItem({ booking, navigation }) {
       }
     } catch (error) {
       console.error("An error occurred while canceling booking:", error);
-      // Show an error message to the user
       Alert.alert("Error", "Failed to cancel booking. Please try again later.");
     }
   };
-  
-
-
   const formatDate = (date) => {
     const options = { day: "numeric", month: "long" };
     return new Date(date).toLocaleDateString(undefined, options);
@@ -77,22 +72,20 @@ function BookingItem({ booking, navigation }) {
         </View>
         <View style={styles.buttonRow}>
           <Button
-            buttonColor="black"
-
             icon="phone"
             mode="contained"
-            style={[styles.button, { backgroundColor: "black" }]}
+            color="black"
+            style={styles.button}
             onPress={handleCallButtonPress}
           >
             Call
           </Button>
           <Button
-            buttonColor="black"
-
             icon="trash-can-outline"
             mode="contained"
-            style={[styles.button, { backgroundColor: "black" }]}
-            onPress={cancelBooking}
+            color="black"
+            style={styles.button}
+            onPress={() => cancelBooking(booking.bookingId)}
           >
             Cancel Booking
           </Button>
