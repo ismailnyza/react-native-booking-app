ce380d0da29389d06fdff7def83656cdb647273a
ismailnyza <ismailnyzarsl@gmail.com>
2023-06-13 01:06:37 +0530

    ipayform

--- Components/IpayForm.js
+++ Components/IpayForm.js
@@ -54,45 +54,40 @@ export default function IpayForm(props) {
   };
 
   const htmlContent = `
-    <html>
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <style>
-          /* CSS styles for your form */
-        </style>
-      </head>
-      <body>
-        <form method="POST" action="https://sandbox.ipay.lk/ipg/checkout">
-          <input type="hidden" name="merchantWebToken" value="eyJhbGciOiJIUzUxMiJ9.eyJtaWQiOiIwMDAwMDQzNyJ9.QRjj-CMOM95XyEhhYPHcKE_wx1nAfpyLlCOCj6hqgSURevSLfWF2_kz7_lX4lrZE4tEPutKVNJCMqRI9mRG03Q">
-          <input type="hidden" name="orderId" value="${sessionUUID}">
-          <input type="hidden" name="returnUrl" value="http://mywebsite.com/return?orderId=OID123456">
-          <input type="hidden" name="cancelUrl" value="http://mywebsite.com/cancel?orderId=OID123456">
-          <table>
-            <tr>
-              <td>Total Amount</td>
-              <td>:</td>
-              <td><input type="text" name="totalAmount" value="750"></td>
-            </tr>
-            <tr>
-              <td>Customer Name</td>
-              <td>:</td>
-              <td><input type="text" name="customerName" value="Ravindu Fernando"></td>
-            </tr>
-            <tr>
-              <td>Customer Mobile</td>
-              <td>:</td>
-              <td><input type="text" name="customerPhone" value="0701234567"></td>
-            </tr>
-            <tr>
-              <td>Customer Email</td>
-              <td>:</td>
-              <td><input type="text" name="customerEmail" value="myemail@mail.com"></td>
-            </tr>
-          </table>
-          <input type="submit" value="Checkout Now">
-        </form>
-      </body>
-    </html>
+  <form method="POST" action="https://sandbox.ipay.lk/ipg/checkout">
+  <input type="hidden" name="merchantWebToken" value="eyJhbGciOiJIUz..."> <!-- Replace your web token -->
+  <input type="hidden" name="orderId" value="OID123456">
+  <input type="hidden" name="orderDescription" value="My Order"> <!-- Optional -->
+  <input type="hidden" name="returnUrl" value="http://mywebsite.com/return?orderId=OID123456">
+  <input type="hidden" name="cancelUrl" value="http://mywebsite.com/cancel?orderId=OID123456">
+  <input type="hidden" name="subMerchantReference" value=""> <!-- Optional -->
+  <table>
+  <tr>
+  <td>Total Amount</td>
+  <td>:</td>
+  <td><input type="text" name="totalAmount" value="750"></td>
+  </tr>
+  <tr>
+  <td>Customer Name </td>
+  <td>:</td>
+  <td><input type="text" name="customerName" value="Ravindu Fernando"></td>
+  </tr>
+  <tr>
+  <td>Customer Mobile</td>
+  <td>:</td>
+  <td><input type="text" name="customerPhone" value="0701234567"></td>
+  </tr>
+  <tr>
+  <td>Customer Email</td>
+  <td>:</td>
+  <td><input type="text" name="customerEmail" value="myemail@mail.com"></td>
+  </tr>
+  </table>
+  <br>
+  <input type="submit" value="Checkout Now">
+  </form>
+  </body>
+  </html>
   `;
 
   return (
