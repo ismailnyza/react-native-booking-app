a80cd7c8acbd96b3c010d520cdc895cc72dba99e
ismailnyza <ismailnyzarsl@gmail.com>
2023-06-13 00:57:47 +0530

    ipayform

--- Components/IpayForm.js
+++ Components/IpayForm.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import { Alert, StyleSheet, View } from "react-native";
 import { WebView } from "react-native-webview";
-import uuid from "react-native-uuid";
+import { v4 as uuidv4 } from "uuid";
 
 const styles = StyleSheet.create({
   container: {
@@ -10,14 +10,12 @@ const styles = StyleSheet.create({
 });
 
 export default function IpayForm(props) {
-  const sessionUUID = uuid.v4();
-  const [currentUrl, setCurrentUrl] = useState("");
+  const sessionUUID = uuidv4();
   const patient = props.route.params;
-  console.log(patient);
+
   const handleNavigationStateChange = (navState) => {
     const { url } = navState;
     if (url != null) {
-      setCurrentUrl(url);
       console.log(url);
     }
 
@@ -27,7 +25,6 @@ export default function IpayForm(props) {
       )
     ) {
       // Payment successful
-      // webViewRef.current.stopLoading();
       fetch("http://192.168.122.1:9090/bookings/create", {
         method: "POST",
         headers: {
@@ -38,10 +35,10 @@ export default function IpayForm(props) {
         .then((response) => {
           if (response.ok) {
             Alert.alert("Payment Successful");
-            // Navigate to MyBookingsScreen
             props.navigation.navigate("Home");
           } else {
             Alert.alert("Failed to create booking");
+            props.navigation.navigate("Home");
           }
         })
         .catch((error) => {
@@ -51,101 +48,52 @@ export default function IpayForm(props) {
         });
     } else if (url === "http://mywebsite.com/cancel?orderId=OID123456") {
       // Payment failed
-      // webViewRef.current.destroy();
       props.navigation.navigate("Home");
       Alert.alert("Transaction Failed");
     }
   };
 
   const htmlContent = `
-  <html>
-    <head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <style>
-        body {
-          margin: 0;
-          padding: 0;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          height: 100vh;
-          background-color: #f2f2f2;
-          font-family: Arial, sans-serif;
-        }
-        
-        form {
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          padding: 20px;
-          background-color: #ffffff;
-          border-radius: 10px;
-          box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-          width: 80%;
-          max-width: 400px;
-        }
-        
-        table {
-          width: 100%;
-          margin-bottom: 20px;
-        }
-        
-        input[type="text"] {
-          width: 100%;
-          padding: 10px;
-          border-radius: 5px;
-          border: 1px solid #dddddd;
-          margin-bottom: 10px;
-          box-sizing: border-box;
-        }
-        
-        input[type="submit"] {
-          background-color: #000000;
-          color: #ffffff;
-          padding: 10px 20px;
-          border-radius: 20px;
-          border: none;
-          font-weight: bold;
-          cursor: pointer;
-          width: 100%;
-        }
-      </style>
-    </head>
-    <body>
-      <form method="POST" action="https://sandbox.ipay.lk/ipg/checkout">
-        <input type="hidden" name="merchantWebToken" value="eyJhbGciOiJIUzUxMiJ9.eyJtaWQiOiIwMDAwMDQzNyJ9.QRjj-CMOM95XyEhhYPHcKE_wx1nAfpyLlCOCj6hqgSURevSLfWF2_kz7_lX4lrZE4tEPutKVNJCMqRI9mRG03Q"> 
-        <input type="hidden" name="orderId" value={"OID" + uuid.v4()}">
-        <input type="hidden" name="returnUrl" value="http://mywebsite.com/return?orderId=OID123456">
-        <input type="hidden" name="cancelUrl" value="http://mywebsite.com/cancel?orderId=OID123456">
-        <table>
-          <tr>
-            <td>Total Amount</td>
-            <td>:</td>
-            <td><input type="text" name="totalAmount" value="750"></td>
-          </tr>
-          <tr>
-            <td>Customer Name</td>
-            <td>:</td>
-            <td><input type="text" name="customerName" value="Ravindu Fernando"></td>
-          </tr>
-          <tr>
-            <td>Customer Mobile</td>
-            <td>:</td>
-            <td><input type="text" name="customerPhone" value="0701234567"></td>
-          </tr>
-          <tr>
-            <td>Customer Email</td>
-            <td>:</td>
-            <td><input type="text" name="customerEmail" value="myemail@mail.com"></td>
-          </tr>
-        </table>
-        <input type="submit" value="Checkout Now">
-      </form>
-    </body>
-  </html>
-`;
-
-
+    <html>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+          /* CSS styles for your form */
+        </style>
+      </head>
+      <body>
+        <form method="POST" action="https://sandbox.ipay.lk/ipg/checkout">
+          <input type="hidden" name="merchantWebToken" value="YOUR_MERCHANT_WEB_TOKEN">
+          <input type="hidden" name="orderId" value="${sessionUUID}">
+          <input type="hidden" name="returnUrl" value="http://mywebsite.com/return?orderId=OID123456">
+          <input type="hidden" name="cancelUrl" value="http://mywebsite.com/cancel?orderId=OID123456">
+          <table>
+            <tr>
+              <td>Total Amount</td>
+              <td>:</td>
+              <td><input type="text" name="totalAmount" value="750"></td>
+            </tr>
+            <tr>
+              <td>Customer Name</td>
+              <td>:</td>
+              <td><input type="text" name="customerName" value="Ravindu Fernando"></td>
+            </tr>
+            <tr>
+              <td>Customer Mobile</td>
+              <td>:</td>
+              <td><input type="text" name="customerPhone" value="0701234567"></td>
+            </tr>
+            <tr>
+              <td>Customer Email</td>
+              <td>:</td>
+              <td><input type="text" name="customerEmail" value="myemail@mail.com"></td>
+            </tr>
+          </table>
+          <input type="submit" value="Checkout Now">
+        </form>
+      </body>
+    </html>
+  `;
 
   return (
     <View style={styles.container}>
@@ -153,6 +101,7 @@ export default function IpayForm(props) {
         source={{ html: htmlContent }}
         style={{ flex: 1 }}
         onNavigationStateChange={handleNavigationStateChange}
+        originWhitelist={["*"]} // Allow loading the external payment gateway
       />
     </View>
   );
